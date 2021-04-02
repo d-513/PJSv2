e315a615be8addae276653e4356a40ffe04bd3fb
dada513 <dada513@protonmail.com>
2021-04-02 12:11:58 +0200

    remove toxic filter as bridge was removed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import Discord from "discord.js";
 import cleverbot from "cleverbot-free";
 import AskPrismarine from "./AskPrismarine";
 import cron from "node-cron";
-import rooms from "../rooms.json";
-import "@tensorflow/tfjs";
-import "@tensorflow/tfjs-node";
-import * as toxicity from "@tensorflow-models/toxicity";
 
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
@@ -23,20 +19,7 @@ client.on("ready", () => {
 
 client.on("message", async (message) => {
   if (message.author.id === client.user.id) return;
-  if (message.author.bot && rooms.includes(message.channel.id)) {
-    // toxicity filter
-    const threshold = 0.9;
-    const model = await toxicity.load(threshold);
-    const results = await model.classify([message.content]);
-    if (results.length === 0) return;
-    let toxic = false;
-    results.forEach((classification) => {
-      if (classification.results[0].match === true) {
-        toxic = true;
-      }
-    });
-    if (toxic) message.react("🤡");
-  }
+
   if (message.mentions.members.has(client.user.id)) {
     const msg = message.content.replace(/<@(.*?)>/, "").trim();
     message.channel.startTyping();
