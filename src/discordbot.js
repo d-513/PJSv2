51e10987e7a31b2a18dea113a509ad478ce863f9
dada513 <dada513@protonmail.com>
2021-04-02 11:43:12 +0200

    Tensorflow Toxicity-Test

--- src/discordbot.js
+++ src/discordbot.js
@@ -4,6 +4,10 @@ import { parse } from "discord-command-parser";
 import * as sql from "./sql";
 import AskPrismarine from "./AskPrismarine";
 import cron from "node-cron";
+import rooms from "../rooms.json";
+import "@tensorflow/tfjs";
+import "@tensorflow/tfjs-node";
+import * as toxicity from "@tensorflow-models/toxicity";
 
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
@@ -20,7 +24,21 @@ client.on("ready", () => {
 });
 
 client.on("message", async (message) => {
-  if (message.author.bot) return;
+  if (message.author.id === client.user.id) return;
+  if (message.author.bot && rooms.includes(message.channel.id)) {
+    // toxicity filter
+    const threshold = 0.9;
+    const model = await toxicity.load(threshold);
+    const results = await model.classify([message.content]);
+    if (results.length === 0) return;
+    let toxic = false;
+    results.forEach((classification) => {
+      if (classification.results[0].match === true) {
+        toxic = true;
+      }
+    });
+    if (toxic) message.react("🤡");
+  }
   if (message.mentions.members.has(client.user.id)) {
     const msg = message.content.replace(/<@(.*?)>/, "").trim();
     const res = await cleverbot(msg);
