2c79aea526c30cbe1e806d12e3f647bbca0d3163
dada513 <dada513@protonmail.com>
2021-04-02 12:19:31 +0200

    added geeksay

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Discord from "discord.js";
 import cleverbot from "cleverbot-free";
 import AskPrismarine from "./AskPrismarine";
 import cron from "node-cron";
+import geeksay from "geeksay";
 
 const client = new Discord.Client({
   intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
@@ -19,8 +20,10 @@ client.on("ready", () => {
 
 client.on("message", async (message) => {
   if (message.author.id === client.user.id) return;
-
-  if (message.mentions.members.has(client.user.id)) {
+  if (message.content.trim().startsWith("geeksay ")) {
+    const content = message.content.trim().slice("geeksay ".length);
+    return message.reply(geeksay(content));
+  } else if (message.mentions.members.has(client.user.id)) {
     const msg = message.content.replace(/<@(.*?)>/, "").trim();
     message.channel.startTyping();
     const res = await cleverbot(msg);
